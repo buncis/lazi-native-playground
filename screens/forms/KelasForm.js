f22f8b6afc13fd538e42747719f64f3ec0d735d8
buncis mamen <buncismamen@gmail.com>
2017-07-06 14:13:31 +0700

    ngerubah alert error make yang default aja daripada library sepi

--- screens/forms/KelasForm.js
+++ screens/forms/KelasForm.js
@@ -2,24 +2,24 @@ import React from 'react';
 import {
   Button,
   View,
-  Text
+  Text,
+  Alert
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import t from 'tcomb-form-native';
 import { connect } from 'react-redux';
 import { saveKelas } from '../../actions';
 import Spinner from 'react-native-loading-spinner-overlay';
-import DropdownAlert from 'react-native-dropdownalert';
 import { NavigationActions } from 'react-navigation';
 
 const Form = t.form.Form;
 
 const List = t.struct({
-  title: t.String,          
-  icon: t.maybe(t.String),  
-  name: t.String,              
-  subtitle: t.String,
-  avatar_url: t.maybe(t.String)
+  title: t.String,            
+  icon: t.String,  
+  name: t.String,                
+  subtitle: t.String,  
+  avatar_url: t.String
 });
 
 const options = {};
@@ -54,7 +54,14 @@ class KelasForm extends React.Component {
         (err) => err.response.json().then(({errors}) => 
           {
             this.setState({loading: false});
-            this.dropdown.alertWithType('error', 'Error', errors.icon.toString());
+            Alert.alert(
+              'Error',
+              errors.icon.toString(),
+              [
+                {text: 'OK', onPress: () => console.log('OK Pressed')},
+              ],
+              { cancelable: false }
+            )
           }
         ) 
       )
@@ -75,8 +82,6 @@ class KelasForm extends React.Component {
           underlayColor='#99d9f4'
           title="Save">
         </Button>
-        <DropdownAlert
-          ref={(ref) => this.dropdown = ref}/>
       </View>
     );
     
