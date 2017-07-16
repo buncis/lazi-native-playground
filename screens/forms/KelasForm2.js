4e8445a6c6cba7c5981bbf6fd332f71e0dada412
buncis mamen <buncismamen@gmail.com>
2017-07-16 21:55:10 +0700

    bisa make redux form tapi ribet harus make render input dulu aduh" http://esbenp.github.io/2017/01/06/react-native-redux-form-immutable-styled-components/

--- screens/forms/KelasForm2.js
+++ screens/forms/KelasForm2.js
@@ -1,53 +1,30 @@
 import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
-import { View,Text } from 'react-native'
-import {
-  Input,
-  Switch
-} from 'react-native-clean-form/redux-form'
-import {
-  ActionsContainer,
-  Button,
-  FieldsContainer,
-  Fieldset,
-  Form,
-  FormGroup,
-  Label,
-} from 'react-native-clean-form'
+import { Field, reduxForm } from 'redux-form'
+import { View, Text, TouchableOpacity, TextInput } from 'react-native'
 
-const onSubmit = (values, dispatch) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(values)
-      resolve()
-    }, 1500)
-  })
+const submit = values => {
+  console.log('submitting form', values)
 }
 
-class FormView extends Component {
-  render() {
-    const { handleSubmit, submitting } = this.props
-    return (
-      <Form>
-        <FieldsContainer>
-          <Fieldset label="Contact details">
-            <Input name="first_name" label="First name" placeholder="John" />
-            <Input name="email" label="Email" placeholder="something@domain.com" />
-          </Fieldset>
-          <Fieldset label="Shipping details" last>
-            <Input name="password" label="Address" placeholder="Hejrevej 33" />
-            <Input name="password_repeat" label="City" placeholder="Copenhagen" />
-            <Switch label="Save my details" border={false} name="save_details" />
-          </Fieldset>
-        </FieldsContainer>
-        <ActionsContainer>
-          <Button icon="md-checkmark" iconPlacement="right" onPress={handleSubmit(onSubmit)} submitting={submitting}>Save</Button>
-        </ActionsContainer>
-      </Form>
-    )
-  }
+const renderInput = ({ input: { onChange, ...restInput }}) => {
+  return <TextInput style={styles.input} onChangeText={onChange} {...restInput} />
 }
+
+const Form = props => {
+  const { handleSubmit } = props
+
+  return (
+    <View>
+      <Text>Email:</Text>
+      <Field name="email" component={renderInput} />
+      <TouchableOpacity onPress={handleSubmit(submit)}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
 
 export default reduxForm({
-  form: 'Form'
-})(FormView)
+  form: 'test'
+})(Form)
